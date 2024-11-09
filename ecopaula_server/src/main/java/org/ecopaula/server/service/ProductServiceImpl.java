9b4f6aa1b58a4fa56ceadc2a349e9e19101c42b5
KatAamand <katrine.aamand@icloud.com>
2024-11-10 00:25:41 +0100

    Implementing productService methods

--- ecopaula_server/src/main/java/org/ecopaula/server/service/ProductServiceImpl.java
+++ ecopaula_server/src/main/java/org/ecopaula/server/service/ProductServiceImpl.java
@@ -1,24 +1,35 @@
 package org.ecopaula.server.service;
 
 import org.ecopaula.server.dto.ProductDTO;
+import org.ecopaula.server.repository.ProductRepository;
+import org.ecopaula.server.util.converters.ProductConverter;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+    private final ProductConverter productConverter;
+
+    public ProductServiceImpl(ProductRepository productRepository, ProductConverter productConverter) {
+        this.productRepository = productRepository;
+        this.productConverter = productConverter;
+    }
+
     @Override
     public ProductDTO getProductById(int id) {
-        return null;
+        return productRepository.findById(id).map(productConverter::toDTO).orElse(null);
     }
 
     @Override
     public List<ProductDTO> getAllProducts() {
-        return List.of();
+        return productRepository.findAll().stream().map(productConverter::toDTO).collect(Collectors.toList());
     }
 
     @Override
     public List<ProductDTO> getProductByAnimalId(int animalId) {
-        return List.of();
+        return productRepository.findProductsByAnimalId(animalId).stream().map(productConverter::toDTO).collect(Collectors.toList());
     }
 }
