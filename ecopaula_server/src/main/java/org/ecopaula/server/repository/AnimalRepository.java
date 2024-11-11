f0338c48aa59bc2d554644e110b37f769e99e734
KatAamand <katrine.aamand@icloud.com>
2024-11-11 20:12:41 +0100

    Adding Spring annotation

--- ecopaula_server/src/main/java/org/ecopaula/server/repository/AnimalRepository.java
+++ ecopaula_server/src/main/java/org/ecopaula/server/repository/AnimalRepository.java
@@ -4,9 +4,11 @@ import org.ecopaula.server.entity.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AnimalRepository extends JpaRepository<Animal, Integer> {
 
     @Query("SELECT a FROM Animal a " +
--- ecopaula_server/src/main/java/org/ecopaula/server/repository/OriginRepository.java
+++ ecopaula_server/src/main/java/org/ecopaula/server/repository/OriginRepository.java
@@ -2,6 +2,8 @@ package org.ecopaula.server.repository;
 
 import org.ecopaula.server.entity.Origin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OriginRepository extends JpaRepository<Origin, Integer> {
 }
